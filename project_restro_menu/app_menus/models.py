14303a7b8802d030750d85bacb1f5e0f9407c069
sixdgt <c4crypt@gmail.com>
2023-06-08 17:45:41 +0545

    django model & migrations

--- project_restro_menu/app_menus/models.py
+++ project_restro_menu/app_menus/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
 # Create your models here.
+class Category(models.Model):
+    category_title = models.CharField(max_length=100)
+    category_code = models.CharField(max_length=20, null=True, blank=True)
+
+    class Meta:
+        db_table = "restro_categories"
+        ordering = ["-category_title"]
+    
+class Menu(models.Model):
+    menu_title = models.CharField(max_length=100)
+    menu_price = models.FloatField(default=0.00)
+    menu_ingredient = models.CharField(max_length=100)
+    menu_category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    menu_img = models.FileField(upload_to="images/menus/", blank=True, null=True)
+    menu_created_at = models.DateTimeField(auto_now_add=True)
+    is_active = models.BooleanField(default=True)
+
+    class Meta:
+        db_table = "restro_menus"
+        ordering = ["-menu_title"]
