70c0e99451425cbec809e7b192a47672941ff90e
sixdgt <c4crypt@gmail.com>
2023-06-14 17:43:51 +0545

    admin panel

--- project_restro_menu/app_menus/admin.py
+++ project_restro_menu/app_menus/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
+from app_menus.models import Category, Menu
 
 # Register your models here.
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ["category_title", "category_code"]
+    search_fields = ["category_title", "category_code"]
+    list_filter = ["category_title",]
+
+
+class MenuAdmin(admin.ModelAdmin):
+    list_display = ("menu_title", "menu_category", "menu_price", "menu_ingredient", "menu_created_at")
+    search_fields = ("menu_title",)
+    list_filter = ("menu_title", "menu_category")
+
+admin.site.register(Category, CategoryAdmin)
+admin.site.register(Menu, MenuAdmin)
+
+# customizing admin panel title and headings
+admin.site.site_title = "Admin Panel" # page sub title
+admin.site.site_header = "RESTRO" # page header
+admin.site.index_title = "RESTRO" # page title
--- project_restro_menu/app_menus/models.py
+++ project_restro_menu/app_menus/models.py
@@ -5,6 +5,9 @@ class Category(models.Model):
     category_title = models.CharField(max_length=100)
     category_code = models.CharField(max_length=20, null=True, blank=True)
 
+    def __str__(self):
+        return self.category_title
+
     class Meta:
         db_table = "restro_categories"
         ordering = ["-category_title"]
@@ -17,6 +20,9 @@ class Menu(models.Model):
     menu_img = models.FileField(upload_to="images/menus/", blank=True, null=True)
     menu_created_at = models.DateTimeField(auto_now_add=True)
     is_active = models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.menu_title + " - " + self.menu_category.category_title
 
     class Meta:
         db_table = "restro_menus"
