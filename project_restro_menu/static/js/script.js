87a38f357f73c833633c4e4474391a181a65d664
sixdgt <c4crypt@gmail.com>
2023-06-29 17:14:16 +0545

    static icons, images & bootstrap offile load with static

--- project_restro_menu/static/js/script.js
+++ project_restro_menu/static/js/script.js
@@ -1,4 +1,14 @@
 function changeText(){
-    let anchor = document.getElementById('anchor');
-    anchor.innerHTML = "View Menus";
+    let btn = document.getElementById('btn');
+    console.log("clicked");
+    btn.innerHTML = "I am changed";
+    btn.style.backgroundColor = "red";
+    btn.style.color = "white";
+    btn.style.width = "100%";
+    btn.style.height = "50px";
+    btn.style.padding = "5px";
+    btn.style.borderBlockStyle = "solid";
+    btn.style.borderWidth = "1px";
+    btn.style.borderBlockColor = "red";
+    btn.style.borderRadius = "5px";
 }
